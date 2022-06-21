5053d2f4aeab28f5ef5c0183bd57beb60a359ca2
Mahbub khan <mahbubk@live.com>
2022-06-21 11:46:43 +0100

    Initial commit

---
+++ src/main/java/com/example/budgetapp/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.budgetapp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
